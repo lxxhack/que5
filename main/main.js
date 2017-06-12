81e02bcb65e3e1c233b7b81d6d9802aa90b71498
lxxhack <1572411660@qq.com>
2017-06-13 00:32:21 +0800

    Create main.js

--- main/main.js
+++ main/main.js
@@ -15,47 +15,46 @@ module.exports = function main() {
 };
 
 class Sequence {
-    var input;
   constructor(input) {
       this.input=input;
     // Write your code here
   }
 
   minimum() {
-      var min=input[0];
-      for (var i=1;i<input.length;i++)
+      var min=this.input[0];
+      for (var i=1;i<this.input.length;i++)
       {
-          if (min>input[i])
+          if (min>this.input[i])
           {
-              min=input[i];
+              min=this.input[i];
           }
       }
       return min;
     // Write your code here
   }
      maximum() {
-      var max=input[0];
-      for (var i=1;i<input.length;i++)
+      var max=this.input[0];
+      for (var i=1;i<this.input.length;i++)
       {
-          if (max<input[i])
+          if (max<this.input[i])
           {
-              max=input[i];
+              max=this.input[i];
           }
       }
       return min;
     // Write your code here
   }
      count() {
-      return input.length;
+      return this.input.length;
     // Write your code here
   }
      avg() {
          var sum=0.0;
-      for (var i=0;i<input.length;i++)
+      for (var i=0;i<this.input.length;i++)
       {
-          sum+=input[i];
+          sum+=this.input[i];
       }
-      sum/=input.length;
+      sum/=this.input.length;
       return sum.toFixed(2);
     // Write your code here
   }
